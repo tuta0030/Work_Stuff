e9d575daf05c327d333edc3f38da70822c343d76
tuta0030 <jokerisfun@126.com>
2020-09-01 13:37:14 +0800

    FENGRUDING_PC分支: 添加pynput，添加通过剪贴板中的内容创建utf8文本文件的函数

--- Auto_work/Mi_ERP_sheet_fix/auto_translate.py
+++ Auto_work/Mi_ERP_sheet_fix/auto_translate.py
@@ -1,2 +1,28 @@
+import pynput
+import pyperclip
+import os
 
 
+def make_txt_hotkey():
+
+    def on_activate():
+        print('Global hotkey activated!')
+        out_path = 'c:\\hotkey_folder'
+        if not os.path.isdir(out_path):
+            os.mkdir(out_path)
+        with open(f'{out_path}\\剪贴板中内容创建的utf-8文件.txt', 'w', encoding='utf-8') as f:
+            f.write(pyperclip.paste())
+        os.startfile(out_path)
+
+    def for_canonical(f):
+        return lambda k: f(listener.canonical(k))
+
+    hotkey = pynput.keyboard.HotKey(pynput.keyboard.HotKey.parse('<ctrl>+<shift>+f'), on_activate)
+
+    with pynput.keyboard.Listener(on_press=for_canonical(hotkey.press),
+                                  on_release=for_canonical(hotkey.release)) as listener:
+        listener.join()
+        listener.stop()
+
+
+make_txt_hotkey()
