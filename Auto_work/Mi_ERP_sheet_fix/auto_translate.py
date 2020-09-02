2ae59dfe73190974bf9b813612207b4e03b9415f
tuta0030 <jokerisfun@126.com>
2020-09-02 12:01:14 +0800

    FENGRUDING_PC分支: 创建线程的代码移动到创建快捷键的函数里

--- Auto_work/Mi_ERP_sheet_fix/auto_translate.py
+++ Auto_work/Mi_ERP_sheet_fix/auto_translate.py
@@ -2,28 +2,33 @@ import pynput
 import pyperclip
 import os
 import pyautogui
+import threading
 
 
 def make_txt_hotkey():
+    """使用剪贴板生成utf-8文本文件的全局快捷键"""
+    def main():
+        def past_content_into_utf_txt():
+            print('Global hotkey activated!')
+            out_path = 'c:\\hotkey_folder'
+            if not os.path.isdir(out_path):
+                os.mkdir(out_path)
+            with open(f'{out_path}\\剪贴板中内容创建的utf-8文件.txt', 'w', encoding='utf-8') as f:
+                f.write(pyperclip.paste())
+            os.startfile(out_path)
 
-    def past_content_into_utf_txt():
-        print('Global hotkey activated!')
-        out_path = 'c:\\hotkey_folder'
-        if not os.path.isdir(out_path):
-            os.mkdir(out_path)
-        with open(f'{out_path}\\剪贴板中内容创建的utf-8文件.txt', 'w', encoding='utf-8') as f:
-            f.write(pyperclip.paste())
-        os.startfile(out_path)
+        def for_canonical(f):
+            return lambda k: f(listener.canonical(k))
 
-    def for_canonical(f):
-        return lambda k: f(listener.canonical(k))
+        hotkey = pynput.keyboard.HotKey(pynput.keyboard.HotKey.parse('<ctrl>+<shift>+f'), past_content_into_utf_txt)
 
-    hotkey = pynput.keyboard.HotKey(pynput.keyboard.HotKey.parse('<ctrl>+<shift>+f'), past_content_into_utf_txt)
+        with pynput.keyboard.Listener(on_press=for_canonical(hotkey.press),
+                                      on_release=for_canonical(hotkey.release)) as listener:
+            listener.join()
+            listener.stop()
 
-    with pynput.keyboard.Listener(on_press=for_canonical(hotkey.press),
-                                  on_release=for_canonical(hotkey.release)) as listener:
-        listener.join()
-        listener.stop()
+    hotkey_thread = threading.Thread(target=main)
+    hotkey_thread.start()
 
 
 def check_bottom() -> bool:
